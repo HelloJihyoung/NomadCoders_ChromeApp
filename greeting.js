1789050fa808dedd41c015156b43d4995f57f86a
hellojihyoung <jihyoungkwon@gmail.com>
2020-07-29 13:00:23 +0900

    no message

--- greeting.js
+++ greeting.js
@@ -6,21 +6,32 @@ const form = document.querySelector(".js-form"), //gets the first one that he fi
 const User_LS = "currentUser",
     Showing_ClassName = "showing";
 
+function saveName(text){
+    localStorage.setItem(User_LS, text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();  
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
 
 function askForName(){
     form.classList.add(Showing_ClassName);
+    form.addEventListener("submit", handleSubmit);
 }
 
 function paintGreeting(text){
     form.classList.remove(Showing_ClassName);
     greeting.classList.add(Showing_ClassName);
-    greeting.innerText = `Hello ${text}`;
+    greeting.innerText = `Hello ${text}!`;
 }
 
 function loadName(){
     const currentUser = localStorage.getItem(User_LS);
     if(currentUser === null){
-
+        askForName();
     } else {
         paintGreeting(currentUser);
     }
