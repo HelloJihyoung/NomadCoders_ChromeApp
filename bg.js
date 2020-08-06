02ea836d27423b5f8b90808dfec9dcaf5414fb24
hellojihyoung <jihyoungkwon@gmail.com>
2020-08-06 17:51:02 +0900

    no message

--- bg.js
+++ bg.js
@@ -2,17 +2,17 @@ const body = document.querySelector("body");
 
 const IMG_NUMBER = 3
 
-function handleImgLoad(){
+/**function handleImgLoad(){
     console.log("finished loading");
-}
+}**/
 
 function paintImage(ImgNumber){
     const image = new Image();
     image.src = `Images/${ImgNumber + 1}.jpg`;
     image.classList.add("bgImage");
     body.prepend(image);
-    image.addEventListener("loadend", handleImgLoad);
-    /**if you working at API you'll need it, cuz it doesn't have loading 
+    /**image.addEventListener("loadend", handleImgLoad);
+    if you working at API you'll need it, cuz it doesn't have loading 
     you don't need it now..!**/
 }
 
